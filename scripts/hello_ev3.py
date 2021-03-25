6cf20687154d0f70fcdcf81138201558c835fecd
Andrew Grankin <andreygrankin@gmail.com>
2021-03-25 22:29:00 +0200

    scripts/hello_ev3.py. Done.

--- scripts/hello_ev3.py
+++ scripts/hello_ev3.py
@@ -5,3 +5,13 @@ import os
 os.system('setfont Lat15-TerminusBold14')
 print('Hello, my name is EV3!')
 Sound.speak('Hello, my name is EV3!').wait()
+
+motorLeft = LargeMotor('outD')
+motorRight = LargeMotor('outA')
+
+motorLeft.stop_action = 'hold'
+motorRight.stop_action = 'hold'
+motorLeft.run_to_rel_pos(position_sp = 840, speed_sp = 250)
+motorRight.run_to_rel_pos(position_sp = -840, speed_sp = 250)
+motorLeft.wait_while('running')
+motorRight.wait_while('running')
