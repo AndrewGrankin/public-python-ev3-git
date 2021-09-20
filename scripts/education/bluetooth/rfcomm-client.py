37b986f8cc9019929738a2fd4e849870d320487a
Andrew Grankin <andreygrankin@gmail.com>
2021-09-20 19:01:34 +0300

    Added rfcomm-client.py. Draft.

---
+++ scripts/education/bluetooth/rfcomm-client.py
@@ -0,0 +1,30 @@
+##!/usr/bin/python3
+
+import bluetooth
+
+target_address = None
+
+print("scanning for nearby bluetooth devices...")
+print("========================================")
+nearby_devices = bluetooth.discover_devices(duration = 20, lookup_names = True)
+
+device_count = len(nearby_devices)
+count = 0
+devices_dict = dict()
+
+if device_count:
+    print("Available devices (" + str(device_count) + "):")
+    for device, name in nearby_devices:
+        count += 1
+        devices_dict[count] = [device, name]
+        print(count, name)
+
+    print("=======================================")
+    print("type a number of the selected device")
+    print("or type 0 to quit")
+
+    choice = int(input())
+    if choice != 0 and choice <= device_count:
+        print("you've selected", str(devices_dict[choice][1]))
+    else:
+        print("you've typted 0 or a number that is greater than amount of available devices")
