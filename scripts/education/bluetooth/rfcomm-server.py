733f708327fbb4a04db31261a7f41286f562ccdb
Andrew Grankin <andreygrankin@gmail.com>
2021-09-22 02:03:37 +0300

    rfcomm-server.py. transfered data successfully.

--- scripts/education/bluetooth/rfcomm-server.py
+++ scripts/education/bluetooth/rfcomm-server.py
@@ -6,3 +6,12 @@ port = 1
 server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 server_socket.bind(("",port))
 server_socket.listen(1)
+
+client_socket,address = server_socket.accept()
+print("Accepted connection from", address)
+
+data = client_socket.recv(1024)
+print("Received [%s]" %data)
+
+client_socket.close()
+server_socket.close()
