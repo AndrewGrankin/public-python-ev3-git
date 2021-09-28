c867f58efb12c9eb6134b4f9312e6934bd5e8d93
Andrew Grankin <andreygrankin@gmail.com>
2021-09-28 18:42:06 +0300

    Added l2cap-client/server.py

---
+++ scripts/education/bluetooth/l2cap-server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import bluetooth
+
+from ev3dev2.sound import Sound
+
+speaker = Sound()
+
+port = 0x1001
+server_socket = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+server_socket.bind(("",port))
+server_socket.listen(port)
+
+client_socket,address = server_socket.accept()
+print("Accepted connection from", address)
+
+data = client_socket.recv(1024)
+while str(data) is not "exit":
+    print("Received:", data)
+    #speaker.speak(str(data))
+    data = client_socket.recv(1024)
+
+client_socket.close()
+server_socket.close()
