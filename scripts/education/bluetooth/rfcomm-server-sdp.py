3cf750b7f673eb1f52e3437e9a2828f8a1594f21
Andrew Grankin <andreygrankin@gmail.com>
2021-09-29 21:37:07 +0300

    Playing with rfcomm-server-sdp.py

--- scripts/education/bluetooth/rfcomm-server-sdp.py
+++ scripts/education/bluetooth/rfcomm-server-sdp.py
@@ -2,13 +2,14 @@
 
 import bluetooth
 
-from ev3dev2.sound import Sound
+#from ev3dev2.sound import Sound
 
-speaker = Sound()
+#speaker = Sound()
 
 server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 print("Getting available RFCOMM port...")
-port = bluetooth.get_available_port(bluetooth.RFCOMM)
+#port = bluetooth.get_available_port(bluetooth.RFCOMM)
+port = 3
 server_socket.bind(("",port))
 server_socket.listen(port)
 print("Listening on port:", port)
@@ -21,7 +22,7 @@ client_socket,address = server_socket.accept()
 print("Accepted connection from", address)
 
 data = client_socket.recv(1024)
-while str(data) is not "exit":
+while str(data) != "exit":
     print("Received:", data)
     #speaker.speak(str(data))
     data = client_socket.recv(1024)
