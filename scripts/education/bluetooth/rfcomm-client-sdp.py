d3d56014e13124f3e674b17c74ebbb398260e104
Andrew Grankin <andreygrankin@gmail.com>
2021-10-01 15:15:45 +0300

    rfcomm-client/server-sdp.py. Done. It works!

--- scripts/education/bluetooth/rfcomm-client-sdp.py
+++ scripts/education/bluetooth/rfcomm-client-sdp.py
@@ -1,27 +1,37 @@
 ##!/usr/bin/python3
 
 import sys
-import bluetooth
+from bluetooth import *
 
+
+addr = None
 uuid = "ffff"
-service_matches = bluetooth.find_service(uuid)
+#uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+service_matches = find_service( uuid = uuid, address = addr )
 
 if len(service_matches) == 0:
     print("Couldn't find any service!")
     sys.exit(0)
 
-print(service_matches)
+print("********************")
+for key, value in service_matches[0].items():
+    print(key, value)
+print("********************")
+
 first_match = service_matches[0]
 port = first_match["port"]
 name = first_match["name"]
 host = first_match["host"]
 
-print("Connecting to \"%s\" on %s", name, host)
-client_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+print("Connecting to", name, "on host", host, "and port", port)
+client_socket = BluetoothSocket( RFCOMM )
 client_socket.connect((host, port))
 
-message = str(input())
-while message != "exit":
+while True:
+    print("Enter some message:")
+    message = str(input())
     client_socket.send(message)
+    if message == "exit":
+        break
 
 client_socket.close()
--- scripts/education/bluetooth/rfcomm-server-sdp.py
+++ scripts/education/bluetooth/rfcomm-server-sdp.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
 
-import bluetooth
-
+from bluetooth import *
 #from ev3dev2.sound import Sound
 
 #speaker = Sound()
 
-server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-print("Getting available RFCOMM port...")
-#port = bluetooth.get_available_port(bluetooth.RFCOMM)
-port = 3
-server_socket.bind(("",port))
-server_socket.listen(port)
+server_socket = BluetoothSocket( RFCOMM )
+server_socket.bind(("",PORT_ANY))
+server_socket.listen(1)
+port = server_socket.getsockname()[1]
+print(server_socket.getsockname())
 print("Listening on port:", port)
 
 uuid = "ffff"
+#uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 print("Advertising service EV3...")
-bluetooth.advertise_service(server_socket, "EV3 Service", uuid)
+advertise_service(server_socket, "EV3_Service",
+                            service_id = uuid,
+                            service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                            profiles = [ SERIAL_PORT_PROFILE ])
+
+print("Waiting for connection of RFCOMM channel %d" % port)
 
 client_socket,address = server_socket.accept()
 print("Accepted connection from", address)
 
-data = client_socket.recv(1024)
-while str(data) != "exit":
+while True:
+    data = client_socket.recv(1024)
+    if len(data) == 0:
+        break
     print("Received:", data)
     #speaker.speak(str(data))
-    data = client_socket.recv(1024)
 
 client_socket.close()
 server_socket.close()
